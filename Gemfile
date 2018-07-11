1d560af6acb2d6fdfd9f7d30e85801f25170512f
Zac Willmington <zac.willmington@gmail.com>
2018-07-11 21:50:03 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'pry'
   gem 'spring'
   gem 'rspec-rails', '~> 3.7'
   gem 'capybara'
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,4 @@
+require "pry"
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
@@ -12,5 +13,23 @@ class RecipesController < ApplicationController
   end
 
   def create
+      binding.pry
+      @recipe = Recipe.new(recipe_params)
+
+      if @recipe.save
+          redirect_to @recipe
+      else
+          render :new
+      end
+  end
+
+  def show
+      @recipe = Recipe.find(params[:id])
+  end
+
+  private 
+
+  def recipe_params
+      params.require(:recipe).permit(:title, :ingredients_attributes => [:name, :ingredients])
   end
 end
