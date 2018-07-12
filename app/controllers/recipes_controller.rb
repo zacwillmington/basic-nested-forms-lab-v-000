f2e617704f3d9c78bbbfd1c0df7591aaacfa5798
Zac Willmington <zac.willmington@gmail.com>
2018-07-12 00:13:22 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -30,6 +30,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-      params.require(:recipe).permit(:title, :ingredients_attributes => [])
+      params.require(:recipe).permit(:title, :ingredients_attributes)
   end
 end
